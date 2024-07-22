e3b6416e665a1e09e1013ba7d8d792de4c2e0ee3
JaeSunEo <wotjs020708@gmail.com>
2024-07-22 10:17:41 +0900

    Initialize Project NoteApp

---
+++ NoteApp/NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by 어재선 on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
