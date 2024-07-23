287023e29b3cff4b0a8160466ef2b39959203fbd
JaeSunEo <wotjs020708@gmail.com>
2024-07-23 14:06:27 +0900

    Firebase 초기화 코드 추가

---
+++ SociallyApp/SociallyApp/SociallyApp.swift
@@ -0,0 +1,27 @@
+//
+//  SociallyAppApp.swift
+//  SociallyApp
+//
+//  Created by 어재선 on 7/23/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct SociallyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
