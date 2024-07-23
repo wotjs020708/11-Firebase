4a34322c37c44fdd42154bdfe83135510de43314
JaeSunEo <wotjs020708@gmail.com>
2024-07-23 15:14:48 +0900

    FeedViewd와 @FirestoreQuery 추가

---
+++ SociallyApp/SociallyApp/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  SociallyApp
+//
+//  Created by 어재선 on 7/23/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
