70c15ca7d4f374716a8ce1eeb580c09fd107ce95
JaeSunEo <wotjs020708@gmail.com>
2024-07-23 15:50:04 +0900

    뷰모델 리팩토리

--- SociallyApp/SociallyApp/AuthViewModel.swift
+++ SociallyApp/SociallyApp/AuthViewModel.swift
@@ -7,21 +7,72 @@
 
 import SwiftUI
 import FirebaseAuth
+import FirebaseFirestore
 import AuthenticationServices
 import CryptoKit
 
 class AuthViewModel: ObservableObject {
     @Published var user: User?
     var currentNonce: String?
-
-
-    func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
+    
+    // MARK: - Sign in with Apple Methods
+    func signInWithApple(request: ASAuthorizationAppleIDRequest) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        request.requestedScopes = [.email]
+        request.nonce = sha256(nonce)
+    }
+    
+    func signInWithAppleCompletion(result: Result<ASAuthorization, any Error>) {
+        switch result {
+        case .success(let authResults):
+            switch authResults.credential {
+            case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIDCredential.identityToken else {
+                    fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                          idToken: idTokenString,
+                                                          rawNonce: nonce)
+                Auth.auth().signIn(with: credential) { (authResult, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    print("signed in")
+                    guard let user = authResult?.user else { return }
+                    
+                    let userData = ["email": user.email, "uid": user.uid]
+                    Firestore.firestore().collection("User")
+                        .document(user.uid)
+                        .setData(userData as [String : Any]) { _ in
+                            print("DEBUG: Did upload user data.")
+                        }
+                }
+                print("\(String(describing: Auth.auth().currentUser?.uid))")
+            default:
+                break
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    // MARK: - Private Methods
+    private func randomNonceString(length: Int = 32) -> String {        precondition(length > 0)
         let charset: [Character] =
-            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
         var result = ""
         var remainingLength = length
-
+        
         while remainingLength > 0 {
             let randoms: [UInt8] = (0 ..< 16).map { _ in
                 var random: UInt8 = 0
@@ -33,23 +84,23 @@ class AuthViewModel: ObservableObject {
                 }
                 return random
             }
-
+            
             randoms.forEach { random in
                 if remainingLength == 0 {
                     return
                 }
-
+                
                 if random < charset.count {
                     result.append(charset[Int(random)])
                     remainingLength -= 1
                 }
             }
         }
-
+        
         return result
     }
-
-    func sha256(_ input: String) -> String {
+    
+    private func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
         let hashedData = SHA256.hash(data: inputData)
         let hashString = hashedData.compactMap {
--- SociallyApp/SociallyApp/SignUpView.swift
+++ SociallyApp/SociallyApp/SignUpView.swift
@@ -14,54 +14,9 @@ struct SignUpView: View {
     
     var body: some View {
         VStack {
-            SignInWithAppleButton(onRequest: { request in
-                let nonce = authModel.randomNonceString()
-                authModel.currentNonce = nonce
-                request.requestedScopes = [.email]
-                request.nonce = authModel.sha256(nonce)
-            }, onCompletion: { result in
-                switch result {
-                case .success(let authResults):
-                    switch authResults.credential {
-                    case let appleIDCredential as ASAuthorizationAppleIDCredential:
-                        guard let nonce = authModel.currentNonce else {
-                            fatalError("Invalid state: A login callback was received, but no login request was sent.")
-                        }
-                        guard let appleIDToken = appleIDCredential.identityToken else {
-                            fatalError("Invalid state: A login callback was received, but no login request was sent.")
-                        }
-                        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-                            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-                            return
-                        }
-                        let credential = OAuthProvider.credential(withProviderID: "apple.com",
-                                                                  idToken: idTokenString,
-                                                                  rawNonce: nonce)
-                        Auth.auth().signIn(with: credential) { (authResult, error) in
-                            if let error = error {
-                                print(error.localizedDescription)
-                                return
-                            }
-                            print("signed in")
-                            guard let user = authResult?.user else { return }
-                            
-                            let userData = ["email": user.email, "uid": user.uid]
-                            Firestore.firestore().collection("User")
-                                .document(user.uid)
-                                .setData(userData as [String : Any]) { _ in
-                                    print("DEBUG: Did upload user data.")
-                                }
-                        }
-                        print("\(String(describing: Auth.auth().currentUser?.uid))")
-                    default:
-                        break
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            })
-            .signInWithAppleButtonStyle(.black)
-            .frame(width: 290, height: 45, alignment: .center)
+            SignInWithAppleButton(onRequest: authModel.signInWithApple(request:),
+                                  onCompletion: authModel.signInWithAppleCompletion(result:))            .signInWithAppleButtonStyle(.black)
+                .frame(width: 290, height: 45, alignment: .center)
         }
     }
 }
