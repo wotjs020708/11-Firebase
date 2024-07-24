e81b6b6803244ad1926c7ec7bdbc5c569d21c5a5
JaeSunEo <wotjs020708@gmail.com>
2024-07-24 13:48:53 +0900

    프로필 이미지 위치 조정

--- SociallyApp/SociallyApp/Accounts/Views/ProFileView.swift
+++ SociallyApp/SociallyApp/Accounts/Views/ProFileView.swift
@@ -23,30 +23,23 @@ struct ProfileView: View {
                     Section("Your account") {
                         HStack {
                             PhotosPicker(selection: $selectedItem, maxSelectionCount: 1, selectionBehavior: .default, matching: .images, preferredItemEncoding: .automatic) {
-                                if let data = data, let image = UIImage(data: data) {
-                                    Image(uiImage: image)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(maxHeight: 80)
-                                } else {
-                                    AsyncImage(url: authModel.user?.photoURL) { phase in
-                                        switch phase {
-                                        case .empty:
-                                            EmptyView()
-                                        case .success(let image):
-                                            image
-                                                .resizable()
-                                                .scaledToFill()
-                                                .frame(width: 80, height: 80)
-                                                .clipped()
-                                        case .failure:
-                                            Image(systemName: "person.circle")
-                                        @unknown default:
-                                            EmptyView()
-                                        }
+                                AsyncImage(url: authModel.user?.photoURL) { phase in
+                                    switch phase {
+                                    case .empty:
+                                        EmptyView()
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 320, height: 200)
+                                            .clipped()
+                                    case .failure:
+                                        Image(systemName: "person.circle")
+                                    @unknown default:
+                                        EmptyView()
                                     }
-                                    Label("Select a picture", systemImage: "photo.on.rectangle.angled")
                                 }
+                                Label("Select a picture", systemImage: "photo.on.rectangle.angled")
                             }.onChange(of: selectedItem) { _, newValue in
                                 guard let item = selectedItem.first else {
                                     return
