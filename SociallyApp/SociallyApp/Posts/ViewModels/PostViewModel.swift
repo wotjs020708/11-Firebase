c83d014b7422c100ec1492001af1334d4a7913f1
JaeSunEo <wotjs020708@gmail.com>
2024-07-24 11:42:55 +0900

    업로드 경로 겹침 문제 해결 > 전역분수를 지역변수로 변경

--- SociallyApp/SociallyApp/Posts/ViewModels/PostViewModel.swift
+++ SociallyApp/SociallyApp/Posts/ViewModels/PostViewModel.swift
@@ -12,35 +12,35 @@ import FirebaseStorage
 
 class PostViewModel: ObservableObject {
     private var databaseReference = Firestore.firestore().collection("Posts")
-    let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
     
-
+    
     func addData(description: String, datePublished: Date, data: Data, completion: @escaping (Error?) -> Void) {
-          storageReference.putData(data, metadata: nil) { metadata, error in
-              if let error = error {
-                  completion(error)
-                  return
-              }
-
-              self.storageReference.downloadURL { url, error in
-                  if let error = error {
-                      completion(error)
-                      return
-                  }
-
-                  guard let downloadURL = url else {
-                      completion(NSError(domain: "URLError", code: 0, userInfo: nil))
-                      return
-                  }
-
-                  self.databaseReference.addDocument(data: [
-                      "description": description,
-                      "datePublished": datePublished,
-                      "imageURL": downloadURL.absoluteString
-                  ]) { error in
-                      completion(error)
-                  }
-              }
+        let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
+        storageReference.putData(data, metadata: nil) { metadata, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            storageReference.downloadURL { url, error in
+                if let error = error {
+                    completion(error)
+                    return
+                }
+                
+                guard let downloadURL = url else {
+                    completion(NSError(domain: "URLError", code: 0, userInfo: nil))
+                    return
+                }
+                
+                self.databaseReference.addDocument(data: [
+                    "description": description,
+                    "datePublished": datePublished,
+                    "imageURL": downloadURL.absoluteString
+                ]) { error in
+                    completion(error)
+                }
+            }
         }
     }
     
