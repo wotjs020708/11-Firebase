9db907317995690e9f30d4373e03285f3a3bcae7
JaeSunEo <wotjs020708@gmail.com>
2024-07-24 14:49:09 +0900

    썸네일 URL 추가(오류있음)

--- SociallyApp/SociallyApp/Posts/ViewModels/PostViewModel.swift
+++ SociallyApp/SociallyApp/Posts/ViewModels/PostViewModel.swift
@@ -15,14 +15,27 @@ class PostViewModel: ObservableObject {
     
     
     func addData(description: String, datePublished: Date, data: Data, completion: @escaping (Error?) -> Void) {
-        let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
-        storageReference.putData(data, metadata: nil) { metadata, error in
+        let fileUUID = UUID().uuidString
+        let storageReference = Storage.storage().reference().child("\(fileUUID)")
+        // 파일 타입 및 MIME 타입 감지
+        let mimeType = detectMimeType(from: data)
+        
+        // 메타데이터 설정
+        let metadata = StorageMetadata()
+        metadata.contentType = mimeType
+        
+        storageReference.putData(data, metadata: metadata) { metadata, error in
             if let error = error {
                 completion(error)
                 return
             }
             
-            storageReference.downloadURL { url, error in
+            // 임의로 10초 대기 ( 썸네일 생성 시간 )
+            Thread.sleep(forTimeInterval: 10)
+            
+            let thumbRef = Storage.storage().reference().child("thumbs/\(fileUUID)_320x200")
+            
+            thumbRef.downloadURL { url, error in
                 if let error = error {
                     completion(error)
                     return
@@ -43,5 +56,30 @@ class PostViewModel: ObservableObject {
             }
         }
     }
+    func detectMimeType(from data: Data) -> String {
+        var c: UInt8 = 0
+        data.copyBytes(to: &c, count: 1)
+        
+        switch c {
+        case 0xFF:
+            return "image/jpeg"
+        case 0x89:
+            return "image/png"
+        case 0x47:
+            return "image/gif"
+        case 0x49, 0x4D:
+            return "image/tiff"
+        case 0x25:
+            return "application/pdf"
+        case 0xD0:
+            return "application/vnd"
+        case 0x46:
+            return "text/plain"
+        default:
+            // 기본값으로 application/octet-stream 사용
+            return "application/octet-stream"
+        }
+    }
+    
     
 }
--- SociallyApp/SociallyApp/Posts/Views/FeedView.swift
+++ SociallyApp/SociallyApp/Posts/Views/FeedView.swift
@@ -15,8 +15,7 @@ struct FeedView: View {
     
     @FirestoreQuery(
         collectionPath: "Posts",
-        predicates: [.order(by: "description", descending: false),
-                     .order(by: "datePublished", descending: true)]
+        predicates: [.order(by: "datePublished", descending: true)]
     ) var posts: [Post]
     
     @State var showingPost: Bool = false
