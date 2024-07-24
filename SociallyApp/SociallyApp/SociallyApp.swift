2bf526e5cedc7a54707661278d09ecdf0c0b788e
JaeSunEo <wotjs020708@gmail.com>
2024-07-24 13:18:55 +0900

    Analytics 추가

--- SociallyApp/SociallyApp/SociallyApp.swift
+++ SociallyApp/SociallyApp/SociallyApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAnalytics
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
@@ -40,7 +41,9 @@ struct SociallyApp: App {
             }
             .environmentObject(authModel)
             .environmentObject(PostViewModel())
-            
+            .onAppear {
+                Analytics.logEvent("launch_app", parameters: nil)
+            }
         }
     }
 }
