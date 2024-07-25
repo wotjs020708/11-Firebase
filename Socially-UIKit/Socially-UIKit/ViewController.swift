7a7c181ac7446730b61f5b9a7d40ff12e7893799
JaeSunEo <wotjs020708@gmail.com>
2024-07-25 09:42:12 +0900

    initialize socially-UIKit

---
+++ Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socially-UIKit
+//
+//  Created by 어재선 on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
