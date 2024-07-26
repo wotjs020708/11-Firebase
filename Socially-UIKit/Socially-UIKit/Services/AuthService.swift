0b7de242fa05bc1cbb6e6f0ac304d983fc27bb08
JaeSunEo <wotjs020708@gmail.com>
2024-07-26 10:28:39 +0900

    AuthService 생성 - 애플 로그인

---
+++ Socially-UIKit/Socially-UIKit/Services/AuthService.swift
@@ -0,0 +1,98 @@
+//
+//  AuthService.swift
+//  Socially-UIKit
+//
+//  Created by 어재선 on 7/26/24.
+//
+
+import Foundation
+import FirebaseAuth
+import AuthenticationServices
+import CryptoKit
+
+
+class AuthService {
+    static let shared = AuthService()
+    private init() {}
+
+    fileprivate var currentNonce: String?
+
+    // 애플로그인
+    func performAppleSignIn(on viewController: UIViewController,
+                            completion: @escaping (Result<User, Error>) -> Void) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = viewController as? ASAuthorizationControllerDelegate
+//        authorizationController.presentationContextProvider = viewController as? ASWebAuthenticationPresentationContextProviding
+        authorizationController.performRequests()
+    }
+
+    // Firebase로 애플 인증 정보 전송
+    func signInWithApple(idToken: String, rawNonce: String,
+                         completion: @escaping (Result<User,Error>) -> Void) {
+        guard let nonce = currentNonce else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid state: A login callback was received, but no login request was sent."])))
+            return
+        }
+
+        let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idToken, rawNonce: rawNonce)
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = authResult?.user {
+                completion(.success(user))
+            }
+        }
+    }
+
+
+
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
+}
